04a29fd7ee08725e1823c3f63543f9db5cee66b2
Garry Guan <gg99@njit.edu>
2016-03-10 20:52:23 -0500

    Fixed merging, need tile gen stuff now

---
+++ src/Tile_define.h
@@ -0,0 +1,10 @@
+#ifndef _TILE_DEFINE_H_
+#define _TILE_DEFINE_H_
+//terrain tile
+#define TILE_GRASS1 1
+//destructable tiles
+#define TILE_TREE   101
+#define TILE_TREE_W 97
+#define TILE_TREE_H 126
+#define PATH_TILE_TREE "images/terrain/terrain_atlas_tree.png"
+#endif
---
+++ src/monster_ai.h
@@ -0,0 +1,34 @@
+#ifndef _MONSTER_H_
+#define _MONSTER_H_
+
+#include "sprite.h"
+#include "entity.h"
+#include "vector.h"
+#include "simple_logger.h"
+
+//states the ai is in
+#define STATE_IDLE		100 // walking around or standing still
+#define STATE_PATROL		101 // walking around
+#define STATE_AGGRO		102 //actively attacking player
+#define STATE_SEEK		103 //trying to find player
+//monster types
+enum Monster{
+	grue,
+};
+extern entity *player;
+
+entity *monster_spawn(int type);
+void monster_think(entity *self);
+void monster_update(entity *self);
+
+#define sprite_grue_filepath "images/Grue/GrueGrin.png"
+#define grue_imageW  56
+#define grue_imageH  71
+#define grue_frameW  101
+#define grue_frameH  127
+
+extern int MONSTER_SPAWN_TIMER;
+
+
+
+#endif
